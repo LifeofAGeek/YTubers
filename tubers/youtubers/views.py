af7fc7a8199a9870567d405bbe3edf71a5f3e374
LifeofAGeek <sethi8001@gmail.com>
2021-03-05 15:42:42 +0530

    added navbar template

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-
+from .models import Youtuber
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-
+ 
 def youtubers(request):
-    pass
+    tubers = Youtuber.objects.order_by('-created_date')
+    data = {
+        'tubers' : tubers,
+    }
+    return render(request, 'youtubers/youtubers.html', data)
 
 def youtubers_detail(request,id):
-    pass
+    tubers = get_object_or_404(Youtuber, pk=id)
+    data = {
+        'tubers' : tubers,
+    }
+    return render(request, 'youtubers/youtuber_detail.html', data)
 
 def search(request):
     pass
